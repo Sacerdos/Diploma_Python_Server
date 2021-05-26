98590389ade8d0fd9a5642a39f2dcd90eeade9f3
IlyaDychkov <ilya.dychkov@gmail.com>
2021-05-26 14:26:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import flask
+import werkzeug
+import numpy as np
+import os
+from keras.models import load_model
+from keras.preprocessing import image
+import datetime
+
+model = load_model(r'my_model.h5')
+
+
+def predict_digit(img):
+    img_path = img
+    img = image.load_img(img_path, target_size=(256, 256))
+    img = img.convert('L')
+    img = np.array(img)
+    img = img / 255.0
+    img = img.reshape(1, 256, 256, 1)
+    res = model.predict(img)
+    os.remove(img_path)
+    return max(res[0])
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    now = datetime.datetime.now()
+    imagefile = flask.request.files['image']
+    filename = werkzeug.utils.secure_filename(imagefile.filename)
+    for_save = filename + str(now.microsecond)
+    imagefile.save(for_save)
+
+    answer = str(round(predict_digit(for_save) * 100, 1))
+    print(answer)
+    return answer
+
+
+app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ testclass.py
@@ -0,0 +1,42 @@
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+import tkinter as tk
+import tkinter.messagebox as mb
+import tkinter.filedialog as fd
+
+def predict_digit(img):
+    model = load_model(r'../DiplomaPythonServer/my_model.h5')
+    img_path = img
+    img = image.load_img(img_path, target_size=(256, 256))
+    img = img.convert('L')
+    img = np.array(img)
+    img = img / 255.0
+    img = img.reshape(1, 256, 256, 1)
+
+    res = model.predict(img)
+    print(np.argmax(res, axis=-1), max(res[0]))
+    print(res)
+    return np.argmax(res), max(res)
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Определение заболеваний по рентгенографии")
+        btn_file = tk.Button(self, text="Выбрать файл рентгенографии грудной клетки",
+                             command=self.choose_file)
+        btn_file.pack(padx=60, pady=60)
+
+    def choose_file(self):
+
+        filename = fd.askopenfilename(title="Открыть файл", initialdir="/",
+                                      filetypes=[("Изображение", ".png .jpg .jpeg")])
+        if filename:
+            print(filename)
+            digit, acc = predict_digit(filename)
+            msg = "Оценочная вероятность пневмонии по данному изображению составляет " + str(int(acc*100))+'%'
+            mb.showinfo("Информация", msg)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
